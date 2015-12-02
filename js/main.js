e1dae6b0c81cb079b1f316f6662242ce08717328
samyoshon <samuelyshon@gmail.com>
2015-12-02 15:47:13 -0800

    hero page finished

--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@ $(document).ready(function() {
 	});
 
 
-	$("nav ul li a[href^='#']").on('click', function(e) {
+	$("a[href^='#']").on('click', function(e) {
 
 		// prevent default anchor click behavior
 		e.preventDefault();
